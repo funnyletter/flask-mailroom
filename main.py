2a6a83abe7c1eeb9d7104a941a828e776cf00a80
Rachel Schirra <schirra.rachel@gmail.com>
2019-06-25 20:21:38 -0700

    Added handling for empty name field, <0 donation amounts.

--- main.py
+++ main.py
@@ -18,18 +18,39 @@ def all():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    # there are too many return statements up in here and I need to
+    # clean that ish up.
+    # By default we return the main page with all donations.
+    output = redirect(url_for('all'))
     if request.method == 'POST':
-        amount = int(request.form['amount'])
+        # Let's make sure the donation amount is a positive integer...
+        less_than_0_error = "Please enter a donation amount greater than 0."
         try:
-            donor = Donor.select().where(Donor.name == request.form['name']).get()
-        except Donor.DoesNotExist:
-            donor = Donor(name=request.form['name'])
-            donor.save()
-        donation = Donation(donor=donor, value=amount)
-        donation.save()
-        return redirect(url_for('all'))
+            amount = int(request.form['amount'])
+        except ValueError:
+            output = render_template('add.jinja2', error=less_than_0_error)
+        if amount <= 0:
+            output = render_template('add.jinja2', error=less_than_0_error)
+        else:
+            name = request.form['name']
+            if name:
+                try:
+                    donor = Donor.select().where(Donor.name == name).get()
+                except Donor.DoesNotExist:
+                    donor = Donor(name)
+                    donor.save()
+                donation = Donation(donor=donor, value=amount)
+                donation.save()
+                output = redirect(url_for('all'))
+            else:
+                output = render_template(
+                    'add.jinja2',
+                    error='Please enter a donor name.'
+                    )
     else:
-        return render_template('add.jinja2')
+        output = render_template('add.jinja2')
+
+    return output
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
